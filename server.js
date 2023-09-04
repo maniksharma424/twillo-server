b1643483c96f860c9253a4500eec87fbd0e1e44d
MANIKSHARMA2000 <115614705+MANIKSHARMA2000@users.noreply.github.com>
2023-09-04 17:21:50 +0530

    url-encoded

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ dotenv.config();
 const port = 9000;
 
 const app = express();
-app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 connectDB();
 app.use(express.json());
 app.use(cors());
